dadb8f794f2da3abacacbdf61869429fe142dd59
ahmane hajar <hajarahmane@gmail.com>
2026-03-06 11:31:07 +0000

    Squash merge feature/squash-demo

--- src/app.js
+++ src/app.js
@@ -3,5 +3,8 @@ console.log("Update from feature branch")
 console.log("Feature FF")
 console.log("Feature NO-FF")
  console.log("Squash 1")
+<<<<<<< HEAD
  console.log("Squash 2")
  console.log("Squash 2")
+=======
+>>>>>>> feature/squash-demo
