89c7e10c185b08d1efee6feb4c1ee63f32cd076a
ahmane hajar <hajarahmane@gmail.com>
2026-03-06 11:33:36 +0000

    Squash merge feature/squash-demo

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ console.log("Feature FF")
 console.log("Feature NO-FF")
  console.log("Squash 1")
 <<<<<<< HEAD
+<<<<<<< HEAD
  console.log("Squash 2")
  console.log("Squash 2")
+=======
+>>>>>>> feature/squash-demo
 =======
 >>>>>>> feature/squash-demo
