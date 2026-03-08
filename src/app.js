a8ab8d067aad60bff6668f0df4b433a5378cfaa3
ahmane hajar <hajarahmane@gmail.com>
2026-03-08 01:07:11 +0000

    Save changes before switching

--- src/app.js
+++ src/app.js
@@ -2,12 +2,5 @@ console.log("Update from A")
 console.log("Update from feature branch")
 console.log("Feature FF")
 console.log("Feature NO-FF")
- console.log("Squash 1")
-<<<<<<< HEAD
-<<<<<<< HEAD
- console.log("Squash 2")
- console.log("Squash 2")
-=======
->>>>>>> feature/squash-demo
-=======
->>>>>>> feature/squash-demo
+
+
